d501d585a6c9eda6a1dffb20bbe8e204d8be8277
Cansu Altunsoy <cansualtunsoy@gmail.com>
2025-12-12 16:48:05 +0300

    cypressv1

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl:"https://uitestingplayground.com/"
+
   },
 });
 
-baseurl:'https://testotomasyonu.com/'
+
---
+++ cypress/e2e/automationCypress/test01.cy.js
@@ -0,0 +1,32 @@
+
+
+describe('Cypress Komutlari', () => {
+
+    it('cy.visit komutu1', () => {
+
+        //cy.visit('/')  // baseUrl tanimli ise kullanimi
+        cy.visit('https://uitestingplayground.com') // base url tanimli degil ise kullanim sekli
+        
+    });
+
+    it('cy.visit komutu2', () => {
+        cy.visit('/resources')
+         
+    });
+
+
+    it('cy.title komutlari', () => {
+
+        cy.visit('/')  // baseUrl adresinw gider
+
+        cy.title().should('equals' , 'UI Test Automation Playground') //title icin tam esitlik kontrolu
+        cy.title().should('include', 'Test')  // istenilen String icerme durumu
+        cy.title().should('contains', 'Automation') // verilen string ifadenin icerilme durumu
+        
+    });
+    
+
+    it('', () => {
+        
+    });
+});
