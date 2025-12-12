769a03a035f464a43ef39682aa67d1c60f609168
Cansu Altunsoy <cansualtunsoy@gmail.com>
2025-12-12 17:04:04 +0300

    cypressV3

--- cypress/e2e/automationCypress/test01.cy.js
+++ cypress/e2e/automationCypress/test01.cy.js
@@ -17,14 +17,24 @@ describe('Cypress Komutlari', () => {
 
     it('cy.title komutlari', () => {
 
-        cy.visit('/')  // baseUrl adresinw gider
+        cy.visit('/')  // baseUrl adresine gider
 
         cy.title().should('equals' , 'UI Test Automation Playground') //title icin tam esitlik kontrolu
-        cy.title().should('include', 'Test')  // istenilen String icerme durumu
+        cy.title().should('include', 'Test')  //  title istenilen String ifadeyi iceriyor mu kontrol eder
         cy.title().should('contains', 'Automation') // verilen string ifadenin icerilme durumu
         
     });
 
 
+    it('cy.url ve cy.location komutlari', () => {
+ 
+        cy.visit('/')  // baseUrl adresine gider
+
+        cy.url().should('equals' , 'https://uitestingplayground.com' ) // url tam esitlik kontorlu saglar
+        cy.url().should('include' , 'uitestingplayground' ) // url istenilen string ifadeyi iceriyor mu kontrolu saglar
+        
+
+        
+    });
    
 });
