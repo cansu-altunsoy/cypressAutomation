df62224544527f30daae20453a381341728f9069
Cansu Altunsoy <cansualtunsoy@gmail.com>
2025-12-12 10:14:45 +0300

    cypressLoading

---
+++ cypress/e2e/0-MyCypress/Test01.cy.js
@@ -0,0 +1,10 @@
+
+
+describe('Cypress Komutlari', () => {
+
+    it('cy.visit komutu', () => {
+
+        cy.visit('https://testotomasyonu.com/') // base url tanımlı değilse kullanim sekli
+        
+    });
+});
