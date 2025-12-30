5fc3104ba49df02d1d4182f0e2b9c5fb04cab827
Cansu Altunsoy <cansualtunsoy@gmail.com>
2025-12-30 21:51:39 +0300

    contex

--- cypress/e2e/automationCypress/test02.cy.js
+++ cypress/e2e/automationCypress/test02.cy.js
@@ -1,5 +1,19 @@
-describe('', () => {
+
+/*describe('', () => {
     it('', () => {
         
     });
+}); */ //Test suiti olusturmak icin describe kullanilir
+    
+// contex fonk ayni islem icin kullanilabilir
+
+context('contex Kullanimi', () => {
+
+    it('title', () => {
+
+        cy.visit('https://uitestingplayground.com')
+        cy.title().should('include', 'playgroud')
+        
+    });
+    
 });
